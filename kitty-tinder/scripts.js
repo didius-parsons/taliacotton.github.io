25fef5fca047fd0ca8002844d6d5d6937ed36622
didius-parsons <justusd+2016@newschool.edu>
2016-02-20 12:46:16 -0500

    Comments displaying some ways to optimize code

--- kitty-tinder/scripts.js
+++ kitty-tinder/scripts.js
@@ -18,6 +18,25 @@ $(document).ready(function() {
 	var discarded = [];
 
 	// push images to the array
+	/**
+	 * This works well. Another clearner option could be to break the data (i.e image / name) and rendering into two places. 
+	 * You can do this by creating a data object that holds your cat info. Such as
+	 var catData = [
+	    {name:"Garfield", img:  "cat-pics/cat1.png"},
+	    {name:"Chloe", img:  "cat-pics/cat2.png"},
+	    {name:"Copernicus", img:  "cat-pics/cat3.png"},
+	    ..........
+	 ];
+	 
+	 * Then you could populate your images with a simple loop
+	 for(var i=0; i<catData.length; i++)
+	 {
+	 	images.push(createImage(catData[i].img, catData[i].img));
+	 }
+	 
+	 * in the above code i = the current cat, and catData[i].img means return the value of the i'th cats img. 
+	 **/
+	 
 	images.push(createImage("cat-pics/cat1.png", "Garfield"));
 	images.push(createImage("cat-pics/cat2.png", "Chloe"));
 	images.push(createImage("cat-pics/cat3.png", "Copernicus"));
@@ -45,7 +64,20 @@ function endFunction(){
 	liked.push(displayed[0]);
 	displayed.splice(0);
 	
-	if(document.getElementById("randomCat")!=undefined) {
+	/**
+	 * This is the native way of checking. Native meaning no libraries. With Jquery you can check with
+	 
+	if( $("#randomCat").length > 0 );
+	
+	* this works cause jQuery returns an array of elements that match the selector ("#randomcat") 
+	* also type checking should be done with 
+	
+	typeof(document.getElementById("randomCat")) != "undefined"
+	
+	* since document.getElementById("randomCat")!=undefined may not always be true
+	*/
+	
+	if(document.getElementById("randomCat")!=undefined ) {
 			document.getElementById("randomCat").remove();
 			console.log("it worked");
 			//console.log("#randomCat existed and was deleted")
@@ -55,6 +87,11 @@ function endFunction(){
 	console.log("liked has " + liked.length + " elements")
 	console.log("images has " + images.length + " elements")
 
+	/**
+	 * since you jQuery on the page you could write this as such
+	 var slideshow = $(".slideshow");
+	 slideshow.css("background-color", "#F5F5F5");
+	 */
 	var slideshow = document.querySelector(".slideshow");
 	slideshow.style.backgroundColor = "#F5F5F5";
 	$(".cat-name" ).empty();
@@ -69,6 +106,10 @@ function endFunction(){
 //------ FIRST RANDOM IMAGE --------//
 
 	var randomCat = images[[Math.floor(Math.random()*images.length)]];
+	
+	/**
+	 * same info as above - you could write this wiht jQuery
+	 */
 	document.getElementById("randomCatImage").appendChild(randomCat);
 	randomCat.id = "randomCat";
 	var name = document.getElementById('cat-name');
@@ -126,6 +167,9 @@ function endFunction(){
 		
 			//----?-----//
 			
+				/**
+				 * its moving the cat image out of images and putting it in a displayed array
+				 */
 				var index = images.indexOf(randomCat);
 				displayed.push(images[[index]]);
 				if (index > -1) {
@@ -133,6 +177,7 @@ function endFunction(){
 				}	
 		
 				num--
+				
 				console.log("displayed has " + displayed.length + " elements")
 				console.log("liked has " + liked.length + " elements")
 				console.log("images has " + images.length + " elements")
@@ -140,7 +185,7 @@ function endFunction(){
 			
 			} else {
 		
-			endFunction();
+				endFunction();
 			
 			}
 			
@@ -176,6 +221,20 @@ function endFunction(){
 			
 		
 			//------ DISPLAY CAT NAME --------//
+			
+			/*
+			* You could abstract all the cat display logic to a display cat fucntion - that would prevent the 
+			* need to have the same logic in two places - which makes maintence better in the long run. 
+			* you could do something like
+			
+			function displayCat(){
+				//display logic goes here that picks a random cat, and displays it
+			}
+			
+			* then take the two like and dislike clicks and make them simply move it to the like or dislike array then call 
+			* the display cat funciton. That way if you need to change how you display cat info you only 
+			* need to do it in 1 place :)
+			*/
 	
 					var name = document.getElementById('cat-name');
 					name.innerHTML = randomCat.title;
